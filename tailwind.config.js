58ad410a5d5f0ea55bb3b13a3006a3e28847419b
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-06-15 19:50:17 +0100

    add assets folder and configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,32 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "gray-20": "#f5f5f5",
+        "gray-100": "#D3D6DB",
+        "gray-200": "#3A4750",
+        "gray-300": "#303841",
+        "primary-100": "#BE3144"
       },
+      screens: {
+        xs: "480px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1280px",
+      },
+      content: {
+        webdevtext: "url('./assets/webdevhome.png')",
+        portfoliotext: "url('./assets/portfolio-text.png')",
+        arrow: "url('./assets/arrow.png')",
+        bang: "url('./assets/bang.png')",
+        drops: "url('./assets/drops.png')",
+        waves: "url('./assets/waves.png')",
+        line: "url('./assets/line.png')",
+        line2: "url('./assets/line2.png')",
+        arrow2: "url('./assets/arrow2.png')",
+        arrow3: "url('./assets/arrow3.png')",
+        shapes: "url('./assets/shapes.png')",
+      }
     },
   },
   plugins: [],
