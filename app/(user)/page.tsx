30a4da70c7be671adb94e544d5b4b1388bad8407
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2024-02-24 16:52:42 +0000

    increase margin between about + contact section

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -68,7 +68,7 @@ const Home = () => {
       <div className="-mt-20">
         <About/>
       </div>
-      <div className="-mt-28 md:-mt-15">
+      <div className="-mt-28 md:-mt-12">
       <Contact />
       </div>
     </section>
