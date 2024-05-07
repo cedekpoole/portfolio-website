9b37874c3fbd6ca51176ded786ac24b501454fe4
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2024-05-07 21:32:30 +0100

    Refactor code: Fix formatting and responsiveness of nav bar

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -11,25 +11,26 @@ import Contact from "./contact/page";
 import Projects from "./projects/page";
 
 // import link and image next component
-import Link  from "next/link";
+import Link from "next/link";
 
 export const revalidate = 60; // revalidate every 60 seconds
 
 const Home = () => {
   return (
-    <section className="gap-16 bg-slate-200 py-32 md:py-24 md:pb-0 scroll-smooth">
+    <section className="gap-16 scroll-smooth bg-slate-200 py-20 sm:py-24 md:pb-0">
       {/* HERO IMAGE AND MAIN HEADER */}
       <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6">
         {/* MAIN HEADER */}
         <div className="md:basis:-3/5 z-10">
           {/* HEADINGS */}
-          <MotionDiv
-          duration={0.5}
-          x={-50}
-          >
+          <MotionDiv duration={0.5} x={-50}>
             <div className="relative">
               <div className="before:absolute before:-left-20 before:-top-20 after:absolute after:-left-8 after:-top-5 after:scale-x-[-1] after:content-bang md:before:content-webdevtext">
-                <img className="mt-4" src={HomeTitle.src} alt="Homepage Title" />
+                <img
+                  className="mt-4"
+                  src={HomeTitle.src}
+                  alt="Homepage Title"
+                />
               </div>
             </div>
             <p className="p-5 text-sm">
@@ -41,9 +42,12 @@ const Home = () => {
           </MotionDiv>
           {/* CONTACT ME BUTTON */}
           <div className="relative mt-4">
-            <MotionDiv duration={1} y={50} className="before:absolute before:-right-8 after:absolute after:-top-5 after:left-52 after:content-bang sm:before:content-arrow">
-              <div
-              >
+            <MotionDiv
+              duration={1}
+              y={50}
+              className="before:absolute before:-right-8 after:absolute after:-top-5 after:left-52 after:content-bang sm:before:content-arrow"
+            >
+              <div>
                 <Link href="/contact">
                   <ActionButton>Contact Me</ActionButton>
                 </Link>
@@ -58,18 +62,22 @@ const Home = () => {
       </div>
       {/* NAME IMG + PROJECT PORTFOLIO TITLE IMG */}
       <div className="relative">
-        <div className="before:absolute before:-bottom-10 before:right-5 md:before:content-portfoliotext before:z-20">
-          <img className="mt-14 hidden md:block" src={HomePageText.src} alt="Homepage Text" />
+        <div className="before:absolute before:-bottom-10 before:right-5 before:z-20 md:before:content-portfoliotext">
+          <img
+            className="mt-14 hidden md:block"
+            src={HomePageText.src}
+            alt="Homepage Text"
+          />
         </div>
       </div>
       <div className="-mt-10 md:mt-10">
         <Projects />
       </div>
       <div className="-mt-28">
-        <About/>
+        <About />
       </div>
       <div className="-mt-28 md:-mt-12">
-      <Contact />
+        <Contact />
       </div>
     </section>
   );
